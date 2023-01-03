38406ca087e9c831c35740a876c88d69dc520eb4
AdinaMubbashir <121837692+AdinaMubbashir@users.noreply.github.com>
2023-01-02 22:04:07 -0500

    Update BoardTest.java

--- Connect Four/src/test/java/connectfour/BoardTest.java
+++ Connect Four/src/test/java/connectfour/BoardTest.java
@@ -11,14 +11,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-/* you will need to add test methods and likely change the
-setup method as well.  The samples that are here are just so that
-you can see how junit works.
-
-Tests are run on build unless specifically excluded with -x test.
-The test results are reported in the reports subfolder of the build directory */
-
-
 public class BoardTest{
     private Board tester;
 
